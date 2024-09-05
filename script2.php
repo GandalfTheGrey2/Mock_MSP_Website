3d7f6d220ade2c93cc8ab2b35ff1d9e79248428c
BruhThisMans <nathan615richie@gmail.com>
2024-09-05 09:39:37 -0500

    Social + Support Update
    Removed the social media page to only have the icons in the footer. The support page now includes the phone number.

--- script2.php
+++ script2.php
@@ -8,14 +8,15 @@
     see to prove I can do SQL. 
     */
     $cname = $_POST['cname'];
+    $phone = $_POST['phone'];
     $subject = $_POST['subj'];
 
     $conn = new mysqli('localhost','root','','test');
     if($conn->connect_error) {
         die('Connection Failed: ' .$conn->connect_error);
     } else {
-        $stmt = $conn->prepare("INSERT INTO support_table(cname, subj");
-        $stmt->bind_param("ss",$cname,$subj);
+        $stmt = $conn->prepare("INSERT INTO support_table(cname, phone, subj");
+        $stmt->bind_param("sis",$cname,$phone,$subj);
         $stmt->execute();
         echo "Support form sent.";
         $stmt->close();
